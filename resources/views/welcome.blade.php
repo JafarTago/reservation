544ab55206c9e1277c860847310bc02a9fbd334c
jafar <jafartago@gmail.com>
2020-06-26 21:01:05 +0800

    ttt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <?php echo $x ?? '' ;?>
+            <?php echo $ggg  ;?>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
